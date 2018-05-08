a9f5eb209875e3364de436c935268b1961a683ed
Maximilian Mikus <maximilianmikus@gmail.com>
2018-05-08 17:09:06 +0200

    Add generic styles and more props.

--- src/lupus-slider-slide.vue
+++ src/lupus-slider-slide.vue
@@ -3,6 +3,7 @@
     <slot/>
   </swiper-slide>
 </template>
+
 <script>
 import {swiperSlide} from 'vue-awesome-swiper';
 
@@ -13,3 +14,22 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .swiper-slide {
+    -webkit-flex-shrink: 0;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    -webkit-transition-property: -webkit-transform;
+    transition-property: -webkit-transform;
+    -o-transition-property: transform;
+    transition-property: transform;
+    transition-property: transform, -webkit-transform;
+  }
+  .swiper-invisible-blank-slide {
+    visibility: hidden;
+  }
+</style>
